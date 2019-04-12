4dabb950e1ffc0fb299edaf3d5206b87b1f62db2
katyal <katyal@172.16.0.2>
2019-04-12 18:58:31 +0530

    First Commit

---
+++ Restful-Implementation/src/main/java/com/restful/websevices/RestfulImplementation/RestfulImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.restful.websevices.RestfulImplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulImplementationApplication.class, args);
+	}
+
+}
