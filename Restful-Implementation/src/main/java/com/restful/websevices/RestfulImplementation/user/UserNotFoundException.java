20d4d7b324c4313420d1190ce94ab07cb52e83aa
ankkatya <ankkatya@WKWIN4362929.global.publicisgroupe.net>
2019-04-23 17:59:18 +0530

    Exception Handling

--- Restful-Implementation/src/main/java/com/restful/websevices/RestfulImplementation/user/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.restful.websevices.RestfulImplementation.user;
-
-public class UserNotFoundException extends RuntimeException {
-
-	public UserNotFoundException(String message) {
-		super(message);
-	}
-
-}
