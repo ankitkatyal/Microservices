cc2f5e4fcb6c756f4d4edff773f499b0e8e4b638
ankkatya <ankkatya@WKWIN4362929.global.publicisgroupe.net>
2019-04-25 16:46:20 +0530

    Added Invalid Request Handlers

--- Restful-Implementation/src/main/java/com/restful/websevices/RestfulImplementation/user/User.java
+++ Restful-Implementation/src/main/java/com/restful/websevices/RestfulImplementation/user/User.java
@@ -2,15 +2,17 @@ package com.restful.websevices.RestfulImplementation.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
 public class User {
 	
 	private Integer id;
 	
-	@Size(min = 2)
+	@Size(min = 2,message="Name should have atleast two characters")
 	private String name;
 	
+	@Past(message = "Date should be less than current date")
 	private Date birthDate;
 	
 	
